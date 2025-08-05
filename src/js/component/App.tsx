c1a57ec079e07161877bc39fc17c8239a59cb4fe
C. M. de Picciotto <d3p1@d3p1.dev>
2025-08-05 03:58:23 +0000

    feat: add model animations [#1]

--- src/js/component/App.tsx
+++ src/js/component/App.tsx
@@ -2,25 +2,61 @@
  * @description App
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
+import {OrbitControls} from '@react-three/drei'
+import {Suspense, useState} from 'react'
 import * as THREE from 'three'
 import {Canvas} from '@react-three/fiber'
-import {Box} from './app/Box.tsx'
-import {CameraControls} from './app/CameraControls.tsx'
-import {RandomObj} from './app/RandomObj.tsx'
+import {Woman} from './app/Woman.tsx'
+
+const ANIMATIONS = ['Play The Guitar', 'Idle', 'Run', 'Dance', 'Pose']
 
 export const App = () => {
+  const [animation, setAnimation] = useState(0)
+
   return (
-    <Canvas
-      dpr={[1, 2]}
-      gl={{antialias: true, outputColorSpace: THREE.SRGBColorSpace}}
-      camera={{position: [5, 4, 4]}}
-    >
-      <CameraControls />
-      <ambientLight intensity={1.5} />
-      <directionalLight position={[10, 10, 10]} intensity={4.5} />
-      <Box position-x={3} />
-      <RandomObj position={[0, 0, 0]} />
-      <Box position-x={-3} />
-    </Canvas>
+    <>
+      <Canvas
+        dpr={[1, 2]}
+        gl={{antialias: true, outputColorSpace: THREE.SRGBColorSpace}}
+        camera={{position: [-2, 2, 4]}}
+        shadows={true}
+      >
+        <OrbitControls />
+
+        <ambientLight intensity={1.5} />
+        <directionalLight
+          position={[10, 10, 10]}
+          intensity={4.5}
+          castShadow={true}
+        />
+
+        <mesh rotation-x={-Math.PI / 2} receiveShadow={true}>
+          <circleGeometry args={[3]} />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <Suspense>
+          <Woman animation={animation} />
+        </Suspense>
+      </Canvas>
+
+      <ul className="fixed bottom-20 right-20 flex flex-col items-center gap-4 w-50">
+        {ANIMATIONS.map((animationLabel, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => {
+                setAnimation(index)
+              }}
+              className={`${
+                animation === index ? 'bg-primary-500' : 'bg-secondary'
+              } text-primary-900 font-black cursor-pointer uppercase text-xs p-4 rounded-2xl w-full text-center hover:bg-primary-500 transition-colors duration-300`}
+            >
+              {animationLabel}
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
